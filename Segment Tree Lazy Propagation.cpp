ee9d5e9d30e5c1f1fd8ac4e3c5622dc5d9e6802b
Daman2461 <142651012+Daman2461@users.noreply.github.com>
2024-12-29 02:05:25 +0530

    Update Segment Tree Lazy Propagation.cpp

--- Segment Tree Lazy Propagation.cpp
+++ Segment Tree Lazy Propagation.cpp
@@ -23,20 +23,7 @@ public:
         seg[ind] =  (seg[2*ind+1]+ seg[2*ind+2]);
         
     }
-    int query(int ind, int low, int high, vector<int>&seg,int l,int r){
-        if(r<low || l>high)return 0;            // no overlap
-        
-        else if(l<= low && r>=high)return seg[ind];  // full overlap    [l low high r]
-        
-        else{                                       // partial overlap
-            int mid = low + (high-low)/2;
-            int left  = query(2*ind+1 , low, mid, seg, l,r);
-            int right  = query(2*ind+2 ,  mid+1,high, seg, l,r);
-            return    (left+right);
-        }
-        
-        
-    }
+    
     
     void update (int ind, int low,int high, int l, int r,int val, vector<int>&seg,vector<int>&lazy){
         
